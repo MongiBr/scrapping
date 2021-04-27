a43fc90292348b6f700384f164335514b0764e2e
Dhairya <dhairya00798@gmail.com>
2021-04-27 21:12:54 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const morgan = require("morgan");
+
+require("dotenv").config();
+
+const router = require("./routes");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello World",
+  });
+});
+
+app.use("/api", router);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
